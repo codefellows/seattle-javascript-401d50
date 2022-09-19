976c56833448a97303bbe25109f56a2ce9abe245
JacobKnaack <jacobknck@gmail.com>
2022-09-19 13:09:37 -0700

    updates configuration

--- class-01/typed-demo/my-first-module/server.js
+++ class-01/typed-demo/my-first-module/server.js
@@ -3,29 +3,28 @@
 const express = require('express');
 const app = express(); // this is our app singleton
 
-
 // request parameter
 app.get('/params/:name', (request, response) => {
-    let name = request.params.name;
-    response.json({
-        name,
-    })
+  let name = request.params.name;
+  response.json({
+    name,
+  });
 });
 
 // query parameters
 app.get('/query', (request, response) => {
-    let name = request.query.name;
-    let role = request.query.role;
-    response.json({
-        name,
-        role,
-    });
-})
+  let name = request.query.name;
+  let role = request.query.role;
+  response.json({
+    name,
+    role,
+  });
+});
 
 app.get('/hello', (request, response) => {
-    response.json({
-        name: 'JACOB',
-    });
+  response.json({
+    name: 'JACOB',
+  });
 });
 
 module.exports = app;
