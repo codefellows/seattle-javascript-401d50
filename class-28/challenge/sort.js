206c042713e102f1d2493865c074f482749c1d27
JacobKnaack <jacobknck@gmail.com>
2022-10-25 19:40:51 -0700

    adds class 28 code challenge

---
+++ class-28/challenge/sort.js
@@ -0,0 +1,17 @@
+function sortYear(movies) {
+  return [];
+}
+
+function sortTitle(movies) {
+  return [];
+}
+
+function inGenre(movies, genre) {
+  return [];
+}
+
+module.exports = {
+  sortYear,
+  sortTitle,
+  inGenre,
+};
---
+++ class-28/challenge/test.js
@@ -0,0 +1,44 @@
+const Movies = require("./movies.js");
+const { inGenre, sortTitle, sortYear } = require("./sort.js");
+
+describe("Sorters", () => {
+  it("can sort movies by year", () => {
+    const movies = sortYear(Movies);
+    expect(movies.map((m) => m.title)).toEqual([
+      "The Cotton Club",
+      "Crocodile Dundee",
+      "Beetlejuice",
+      "The Shawshank Redemption",
+      "Memento",
+      "City of God",
+      "Ratatouille",
+      "Stardust",
+      "Valkyrie",
+      "The Untouchables",
+    ]);
+  });
+
+  it("can sort movies by title", () => {
+    const movies = sortTitle(Movies);
+    expect(movies.map((m) => m.title)).toEqual([
+      "Beetlejuice",
+      "City of God",
+      "The Cotton Club",
+      "Crocodile Dundee",
+      "The Intouchables",
+      "Memento",
+      "Ratatouille",
+      "The Shawshank Redemption",
+      "Stardust",
+      "Valkyrie",
+    ]);
+  });
+
+  it("can find movies by genre", () => {
+    const movies = sortYear(inGenre(Movies, "Adventure"));
+    expect(movies.map((m) => m.title)).toEqual([
+      "Crocodile Dundee",
+      "Stardust",
+    ]);
+  });
+});
